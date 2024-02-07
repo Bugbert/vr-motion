6777c148626c75b376ee907603b122f792668206
Avery Andrews <avery.andrews@mailfence.com>
2024-02-06 19:05:35 -0500

    Fixed the cube generation code. Also removed unused pannel obscuring the test scene.

--- components/procedural_cubes.gd
+++ components/procedural_cubes.gd
@@ -1,23 +1,14 @@
 class_name ProceduralCubeShape
 extends Node3D
 
-@export var branch_count = 4
+@export var branch_count = 3
 @export var max_branch_len = 5
 @export var size = Vector3(10, 10, 10)
 
+const ADJACENT_VECS = [Vector3(1, 0, 0), Vector3(0, 1, 0), Vector3(0, 0, 1)]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# This creates a simple array of Vectors that are adjacent to Vector3.ZERO.
-	# The first 2 are X, second 2 Y, etc. This means that the index divided by 2
-	# rounded down is the Axis
-	var adjacent_vecs: Array[Vector3]
-	adjacent_vecs.resize(6)
-	for axis in range(3):
-		for n in [-1, 1]:
-			var vec = Vector3.ZERO
-			vec[axis] = n
-			adjacent_vecs[axis*2+max(0, n)] = vec
-
 	# It's simplist to create the shape as an array of vectors first.
 	# The "root" vector is at the origin.
 	var cube_vecs: Array[Vector3] = [Vector3.ZERO]
@@ -27,11 +18,12 @@ func _ready():
 		while !open_axises.size():
 			# Choose a random vec from which to branch from.
 			joint = cube_vecs[randi() % cube_vecs.size()]
-			for j in range(adjacent_vecs.size()):
-				var axis = floor(j/2)
-				if !(joint + adjacent_vecs[j] in cube_vecs) && !(axis in open_axises):
-					open_axises.append(axis)
-		var axis = randi() % open_axises.size()
+			for j in range(3):
+				var up_free = !(joint - ADJACENT_VECS[j] in cube_vecs)
+				var down_free = !(joint + ADJACENT_VECS[j] in cube_vecs)
+				if up_free && down_free:
+					open_axises.append(j)
+		var axis = open_axises[randi() % open_axises.size()]
 		var branch_len: int
 		while !branch_len:
 			branch_len = randi_range(-max_branch_len, max_branch_len)
