b72b1bacdb137b8310aecc8c643f9cc4a79ba0bb
Avery Andrews <avery.andrews@mf.me>
2024-03-25 13:46:27 -0400

    Changed back to vulkan for demo and improved character controls

--- components/character.gd
+++ components/character.gd
@@ -1,11 +1,13 @@
 extends CharacterBody3D
 
-
 @export var speed = 5.0
+@export var sensitivity = .005
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -16,3 +18,8 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * sensitivity)
+		$Camera3D.rotate_x(-event.relative.y * sensitivity)
