dda3049b75b78e4bd24542f525379265696d85cd
Avery Andrews <avery.andrews@mf.me>
2024-03-28 07:49:30 -0400

    Fixed point and click stuff

--- scenes/test_3d_scene.gd
+++ scenes/test_3d_scene.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+@onready var options = [$Option1, $Option2, $Option3, $Option4]
+@onready var player = $Player
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
@@ -17,7 +20,27 @@ func _ready():
 	
 	print(real_shape_index)
 
-
-
 func _process(_delta):
-	pass
+	for option in options:
+		var relative_pos = player.global_position - option.global_position
+		var theta = atan(relative_pos.x / relative_pos.z)
+		if relative_pos.z > 0:
+			option.global_rotation.y = theta
+		else:
+			option.global_rotation.y = theta + PI
+
+
+func _on_option_1_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_option_2_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_option_3_area_entered(area):
+	pass # Replace with function body.
+
+
+func _on_option_4_area_entered(area):
+	pass # Replace with function body.
--- selection_button.gd
+++
@@ -1,9 +0,0 @@
-extends Area3D
-
-func _ready():
-	set_process_input(true)
-	
-func _on_input_event(camera, event, position, normal, shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("Button clicked!")
-		# Add your custom logic here
